79585f2ec8c8d36eda936708e469465feea7f9ea
melhac <melahackl@web.de>
2019-05-13 22:35:04 +0200

    CHange @Main

--- main.R
+++ main.R
@@ -63,3 +63,30 @@ qqnorm(fab_subsets$sub_up_with_linAcc$magnitude)
 qqnorm(fab_subsets$sub_down_without_linAcc$magnitude)
 qqnorm(fab_subsets$sub_down_with_linAcc$magnitude)
 
+#boxplot 
+box_plots(fab_subsets$sub_up_without_linAcc,fab_subsets$sub_up_with_linAcc,fab_subsets$sub_down_without_linAcc,fab_subsets$sub_down_with_linAcc)
+
+#hist vs. ECDF
+par(mfrow=c(2,2))
+test_hist(fab_subsets$sub_up_without_linAcc,"fab_hist_up_without_mag")
+plot.ecdf(fab_subsets$sub_up_without_linAcc$magnitude)
+test_hist(fab_subsets$sub_up_with_linAcc,"fab_hist_up_with_mag")
+plot.ecdf(fab_subsets$sub_up_with_linAcc$magnitude)
+
+#stripcharts under development
+stripchart(round(fab_subsets$sub_up_without_linAcc$magnitude,2) ~fab_subsets$sub_up_without_linAcc$statusId, method="stack")
+
+stripchart(fab_subsets$sub_up_without_linAcc$magnitude ~ fab_subsets$sub_up_without_linAcc$statusId, method="jitter", jitter=0.3)
+
+
+#T test down
+mean_down_with <-c(mean(fab_subsets$sub_down_with_linAcc$magnitude),mean(mel_subsets$sub_down_with_linAcc$magnitude),mean(jua_subsets$sub_down_with_linAcc$magnitude))
+mean_down_without <-c(mean(fab_subsets$sub_down_without_linAcc$magnitude),mean(mel_subsets$sub_down_without_linAcc$magnitude),mean(jua_subsets$sub_down_without_linAcc$magnitude))
+#t test: 
+t.test(mean_down_with,mean_down_without, alternative = "less", paired = TRUE)
+
+#T test up 
+mean_up_with <-c(mean(fab_subsets$sub_up_with_linAcc$magnitude),mean(mel_subsets$sub_up_with_linAcc$magnitude),mean(jua_subsets$sub_up_with_linAcc$magnitude))
+mean_down_without <-c(mean(fab_subsets$sub_up_without_linAcc$magnitude),mean(mel_subsets$sub_up_without_linAcc$magnitude),mean(jua_subsets$sub_up_without_linAcc$magnitude))
+#t test: 
+t.test(mean_down_without,mean_up_with, alternative = "less", paired = TRUE)
