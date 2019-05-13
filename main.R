1c6cd993e72a49edd90e0ec277d96d4260ced96a
melhac <melahackl@web.de>
2019-05-13 19:10:18 +0200

    Change @Main: Read Feasibility Data

--- main.R
+++ main.R
@@ -4,5 +4,12 @@ source("./3_testing/test.R")
 
 stairsData <- read.table("./z_data/stairs_prepro", header = TRUE, sep="|")
 subsets <- preprocess()
-show_overview()
 test_plot(subsets$sub_up_without, subsets$sub_up_with)
+
+#Reading the different datasets:
+mel_data <- read.table("./z_data/stairsMelanie_prepro", header = TRUE, sep="|")
+fab_data <- read.table("./z_data/stairsFabian_prepro", header = TRUE, sep="|")
+jua_data <- read.table("./z_data/stairsJuan_prepro", header = TRUE, sep="|")
+
+#preprocess the data 
+mel_subsets <- preprocess()
